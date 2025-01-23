e06e542354291a8cf8ca58378f7ae290410b02bb
ryanchall99 <rileychall@hotmail.co.uk>
2025-01-23 13:17:01 +0000

    Added Cell Data
    - Created a class for cell data within board manager, that currently stores whether the cell is passable or not.
    - The m_BoardData array is initialized within start with the board width & height as the array size, so all cell data can be stored.
    - Each cell data is created within the loop, and the passable value is set to either true or false based on whether the tile is a wall or ground tile.

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -3,41 +3,44 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
+    public class CellData
+    {
+        public bool passable;
+    }
+
     [SerializeField] int boardWidth;
     [SerializeField] int boardHeight;
     [SerializeField] Tile[] groundTiles;
     [SerializeField] Tile[] wallTiles;
 
     private Tilemap m_Tilemap;
+    private CellData[,] m_BoardData;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
+        m_BoardData = new CellData[boardWidth, boardHeight]; // Initializing with correct array size for all each cell data to be stored
 
         for (int y = 0; y < boardHeight; y++)
         {
             for (int x = 0; x < boardWidth; x++)
             {
                 Tile tile;
+                m_BoardData[x, y] = new CellData(); // Creating each cell data
 
                 if (x == 0 || y == 0 || x == boardWidth - 1 || y == boardHeight - 1)
                 {
                     tile = wallTiles[Random.Range(0, wallTiles.Length)];
+                    m_BoardData[x, y].passable = false;
                 }
                 else
                 {
                     tile = groundTiles[Random.Range(0, groundTiles.Length)];
+                    m_BoardData[x, y].passable = true;
                 }
 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
