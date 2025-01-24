5919bd545ff601c110d8e3ef0271c4aece1683cc
ryanchall99 <rileychall@hotmail.co.uk>
2025-01-24 19:42:32 +0000

    Added Food AI
    - Created a UIDoucment GameObject & Visual Tree Asset.
    - The visual tree asset places a label at the bottom of the screen.
    - Within the game manager, the label is updated with text showing the food count.
    - This label is updated each time the tick event (OnTurnHappen method) is invoked.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    [Header("Board & Player")]
     [SerializeField] BoardManager boardManager;
     [SerializeField] PlayerController player;
 
+    [Header("UI")]
+    [SerializeField] UIDocument UIDoc;
+
     private int m_FoodCount = 100;
+    private Label m_FoodLabel;
 
     public TurnManager turnManager { get; private set; }
 
@@ -29,12 +35,15 @@ public class GameManager : MonoBehaviour
         
         // Board & Player Initialization
         boardManager.Init();
-        player.Spawn(boardManager, new Vector2Int(1, 1));   
+        player.Spawn(boardManager, new Vector2Int(1, 1));
+
+        m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel");
+        m_FoodLabel.text = "Food : " + m_FoodCount;   
     }
 
     private void OnTurnHappen()
     {
         m_FoodCount -= 1;
-        Debug.Log("Food Count: " + m_FoodCount);
+        m_FoodLabel.text = "Food : " + m_FoodCount;
     }
 }
